669545e42dd8a2aaac21cdb4da54043b10373b85
Deepak Kumar <deepak.2002.02.16@gmail.com>
2022-12-24 09:02:45 +0530

    nft fetch, collection fetch, nft transacrtion fetch added

--- src/routes/nfts/mod.rs
+++ src/routes/nfts/mod.rs
@@ -1,2 +1,9 @@
-pub mod get_nfts_by_wallet;
+
+mod nft_fetch_error;
+mod get_nfts_by_wallet;
+mod get_nft_collections_by_wallet;
+mod get_nft_transfers_by_wallet;
+pub use nft_fetch_error::*;
 pub use get_nfts_by_wallet::*;
+pub use get_nft_collections_by_wallet::*;
+pub use get_nft_transfers_by_wallet::*;
